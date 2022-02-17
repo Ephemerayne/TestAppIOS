bb798b3664e0b1e7adcf8690c37365a5a2911bf6
ephemerayne <nika_landers@mail.ru>
2022-02-17 14:19:52 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Veronika Bekesh on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
